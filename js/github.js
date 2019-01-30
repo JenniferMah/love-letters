146a9593e6db129b769f8529277595755d8e7f1d
Jennifer Mah <jema5807@colorado.edu>
2019-01-29 19:47:54 -0700

    adding github

--- js/github.js
+++ js/github.js
@@ -1,5 +1,13 @@
-$("#flipbook").turn({
-    width: 400,
-    height: 300,
-    autoCenter: true
-});
+function open_card() {
+  document.getElementById('front').className = 'open-card';
+}
+
+function close_card() {
+  document.getElementById('front').className = '';
+}
+
+var button=document.getElementById("front");
+button.addEventListener("click", open_card, false);
+
+var button=document.getElementById("inside");
+button.addEventListener("click", close_card, false);
